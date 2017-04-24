c45b4c38647ba5eba353d02d8c6d4c5867463e29
e-bzrn <e-bzrn@GPLPL0059>
2017-04-24 11:22:05 +0200

    change

---
+++ ACM.BL/CustomerRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+   public class CustomerRepository
+    {
+
+        public Customer Retrieve(int customerId)
+        {
+
+            var customer = new Customer(customerId);
+
+            if (customerId==1)
+            {
+                customer.EmailAddress = "jan.nowak@gmail.com";
+                customer.FirstName = "Jan";
+                customer.LastName = "Nowak";
+
+            }
+            return customer;
+        }
+
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+
+        public bool Save()
+        {
+            return true;
+        }
+
+    }
+}
